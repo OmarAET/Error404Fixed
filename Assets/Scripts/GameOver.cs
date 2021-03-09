358bbf0565a0969135ac8b36c64db8ddb9287d3a
omar ali <oa78660@gmail.com>
2021-03-08 21:49:58 -0600

    GameOver

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameOver : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+}
